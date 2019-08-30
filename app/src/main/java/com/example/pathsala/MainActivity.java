62406b4c32f5b0a3ff8c3eb9be46797765a67995
Bikranta <dhamalabikranta@gmail.com>
2019-08-29 22:25:37 -0400

    added second firebase cloud stone

--- app/src/main/java/com/example/pathsala/MainActivity.java
+++ app/src/main/java/com/example/pathsala/MainActivity.java
@@ -2,13 +2,28 @@ package com.example.pathsala;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button ebookButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ebookButton = findViewById(R.id.button);
+        ebookButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(),Options_Activity.class);
+                startActivity(i);
+            }
+        });
+
+
     }
 }
---
+++ app/src/main/java/com/example/pathsala/Options_Activity.java
@@ -0,0 +1,15 @@
+package com.example.pathsala;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Options_Activity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_options);
+    }
+}
